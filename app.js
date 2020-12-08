02a95634f4455bf3bbfc70f90e3c6762acbf8cfc
Sam Einsidler <sameinsidler@me.com>
2020-12-08 10:26:46 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const apps = require("./google-play.js");
+
+app.use(morgan("common"));
+
+app.get("/apps", (req, res) => {
+  res.json(apps);
+});
+
+app.listen(8003, () => {
+  console.log("Server started at http://localhost:8003");
+});
